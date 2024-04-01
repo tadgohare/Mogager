79831eba9c3bc76b6821e1838ffe8e2106874af5
tadgohare <tadgohare@cmail.carleton.ca>
2024-04-01 15:31:35 -0400

    changed lots of stuff

--- src/App.js
+++ src/App.js
@@ -2,9 +2,31 @@ import React from "react";
 
 const App = () =>{
     return (
-        <h1>
-            Mogager
-        </h1>
+        <div>
+            <h1>Mogager</h1>
+            <img src="/mogtrump.webp" alt="mogged"></img>
+            <UserRegistration />
+        </div>
+    )
+}
+
+const UserRegistration = () =>{
+    return (
+        <div className="container">
+            <h2>Start Looskmaxxing Today!</h2>
+            <form>
+                <input type="text" placeholder="Username"></input>
+                <input type="password" placeholder="Password"></input>
+                <input type="password" placeholder="Confirm Password"></input>
+                <div>
+                    <input type="radio" id="male" name="gender" value="male" />
+                    <label for="male">Male</label>
+                    <input type="radio" id="female" name="gender" value="female" />
+                    <label for="female">Female</label>
+                </div>
+                <button>Register</button>
+            </form>
+        </div>
     )
 }
 
